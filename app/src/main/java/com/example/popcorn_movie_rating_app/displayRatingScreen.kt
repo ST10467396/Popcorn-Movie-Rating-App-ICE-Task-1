b17a10d943202ea01d267be6ac662d983f5335a6
ST10467396 <ST10467396@myemeris.edu.za>
2026-04-02 20:05:18 +0200

    Connect Android Studio code to Github
    Added the files from Android Studio to Github. All further added code will be submitted to Github and updated through commits.

---
+++ app/src/main/java/com/example/popcorn_movie_rating_app/displayRatingScreen.kt
@@ -0,0 +1,20 @@
+package com.example.popcorn_movie_rating_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class displayRatingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_display_rating_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
