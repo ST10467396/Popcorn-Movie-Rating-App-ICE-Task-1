1675ba3f704bcfdffe5bd542cac0a2ba92bc0c4e
ST10467396 <ST10467396@myemeris.edu.za>
2026-04-15 03:36:30 +0200

    Code Update
    Added error handeling to the project.

--- app/src/main/java/com/example/popcorn_movie_rating_app/MainActivity.kt
+++ app/src/main/java/com/example/popcorn_movie_rating_app/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,17 +35,28 @@ class MainActivity : AppCompatActivity() {
         //Declare a variable for the name of the movie
         var movieName: String
 
+        //Declare the warning message that will appear when no name is entered
+        val warning = findViewById<TextView>(R.id.txtNameWarning)
+
         //PROCESSING - Navigate to the display screen when pressed
         display.setOnClickListener {
-            //Navigate to the second screen, the display screen
-            startActivity(intent)
-
             //Set the var name of movie to the constant movie name input we got from the user
             movieName = name.text.toString()
 
-            //Display the movie name on the second screen
-            intent.putExtra("movieNameDisplay",movieName)
+            //Error handling - Check if the user entered a movie name
+            if (movieName==""){ //if user did not enter a movie name an error message will pop up
+                warning.isVisible = true
+            }else{ //if user did enter a movie name no message will pop up
+                //Display the movie name on the second screen
+                intent.putExtra("movieNameDisplay", movieName)
+                //Navigate to the second screen, the display screen
+                startActivity(intent)
+            }
+
+
         }
 
     }
 }
+//References
+//<https://notesjam.com/pick-image-from-gallery-in-android/>
