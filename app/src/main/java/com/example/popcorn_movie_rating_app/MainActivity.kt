56cc9a7125f60f68e941f0de1de7047e2dfd3498
ST10467396 <ST10467396@myemeris.edu.za>
2026-04-15 03:22:15 +0200

    Code Update
    Navigation code has been added as well as the code to display the film name on the display screen.

--- app/src/main/java/com/example/popcorn_movie_rating_app/MainActivity.kt
+++ app/src/main/java/com/example/popcorn_movie_rating_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.popcorn_movie_rating_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        //DECLARATIONS - Declare the constants and Variables
+        //Declare the constant that will store the name of the movie
+        val name = findViewById<EditText>(R.id.edtName)
+
+        //Declare the button that will navigate the screen to the display screen
+        val display = findViewById<Button>(R.id.btnCheckRating)
+
+        //Declare the intent for the display screen
+        val intent = Intent(this, displayRatingScreen::class.java)
+
+        //Declare a variable for the name of the movie
+        var movieName: String
+
+        //PROCESSING - Navigate to the display screen when pressed
+        display.setOnClickListener {
+            //Navigate to the second screen, the display screen
+            startActivity(intent)
+
+            //Set the var name of movie to the constant movie name input we got from the user
+            movieName = name.text.toString()
+
+            //Display the movie name on the second screen
+            intent.putExtra("movieNameDisplay",movieName)
+        }
+
     }
 }
--- app/src/main/java/com/example/popcorn_movie_rating_app/displayRatingScreen.kt
+++ app/src/main/java/com/example/popcorn_movie_rating_app/displayRatingScreen.kt
@@ -1,6 +1,8 @@
 package com.example.popcorn_movie_rating_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,21 @@ class displayRatingScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Back btn
+        val back = findViewById<Button>(R.id.btnBack)
+
+        //PROCESSING - Goes back to the main form
+        back.setOnClickListener {
+            finish()
+        }
+        //Display the name of the movie
+        val nameDisplay = findViewById<TextView>(R.id.txtMovieName)
+
+        //get  the name from bundle
+        val bundle: Bundle? = intent.extras
+        val displayMovieName: String?=bundle?.getString("movieNameDisplay")
+        nameDisplay.text = displayMovieName
+
+
     }
 }
