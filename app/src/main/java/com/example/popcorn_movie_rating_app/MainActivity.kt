b085c6191b17d66e2b449858f95ea870696e868d
ST10467396 <ST10467396@myemeris.edu.za>
2026-05-15 23:58:59 +0200

    Code Attribution Update
    Added code attribution to the project and added some code that was missing.

--- app/src/main/java/com/example/popcorn_movie_rating_app/MainActivity.kt
+++ app/src/main/java/com/example/popcorn_movie_rating_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,43 +21,88 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
+
         //DECLARATIONS - Declare the constants and Variables
         //Declare the constant that will store the name of the movie
-        val name = findViewById<EditText>(R.id.edtName)
+        val name = findViewById<EditText>(R.id.edtName)//(Mishka Dewlok,2026)
 
         //Declare the button that will navigate the screen to the display screen
-        val display = findViewById<Button>(R.id.btnCheckRating)
+        val display = findViewById<Button>(R.id.btnCheckRating)//(Mishka Dewlok,2026)
 
         //Declare the intent for the display screen
-        val intent = Intent(this, displayRatingScreen::class.java)
+        val intent = Intent(this, displayRatingScreen::class.java)//(Mishka Dewlok,2026)
 
         //Declare a variable for the name of the movie
-        var movieName: String
+        var movieName: String//(Mishka Dewlok,2026)
 
         //Declare the warning message that will appear when no name is entered
-        val warning = findViewById<TextView>(R.id.txtNameWarning)
+        val warning = findViewById<TextView>(R.id.txtNameWarning)//(Mishka Dewlok,2026)
+
+
+        //Declare the movie rating value
+        val ratingInput = findViewById<TextView>(R.id.txtRating)//(Mishka Dewlok,2026)
+        var rating: String//(Mishka Dewlok,2026)
+
+        //Declare the warning message if the user entered the wrong rating
+        val error = findViewById<TextView>(R.id.txtRatingWarning)//(Mishka Dewlok,2026)
 
         //PROCESSING - Navigate to the display screen when pressed
         display.setOnClickListener {
+
             //Set the var name of movie to the constant movie name input we got from the user
-            movieName = name.text.toString()
+            movieName = name.text.toString()//(Mishka Dewlok,2026)
+            //var isRated: Boolean = false
+            ////Set the var value of the rating to the constant rating we got from the user
+            rating = ratingInput.text.toString()//(Mishka Dewlok,2026)
+            //Error handling - check that the user entered a valid rating
+
 
             //Error handling - Check if the user entered a movie name
             if (movieName==""){ //if user did not enter a movie name an error message will pop up
-                warning.isVisible = true
+                warning.isVisible = true//(Mishka Dewlok,2026)
             }else{ //if user did enter a movie name no message will pop up
                 //Display the movie name on the second screen
-                intent.putExtra("movieNameDisplay", movieName)
+                intent.putExtra("movieNameDisplay", movieName)//(Mishka Dewlok,2026)
+                intent.putExtra("rating",rating)//(Mishka Dewlok,2026)
                 //Navigate to the second screen, the display screen
-                startActivity(intent)
-            }
+                startActivity(intent)//(Mishka Dewlok,2026)
+            }//(Mishka Dewlok,2026)
 
 
         }
 
     }
 }
-//References
-//<https://notesjam.com/pick-image-from-gallery-in-android/>
+//References - Code Attribution
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260305_180248-Meeting Recording.mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5411078>
+//[Accessed Date 11 May 2026].
+
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260319_180645-Meeting Recording.mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5488548>
+//[Accessed Date 11 May 2026].
+
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260402_181545-Meeting Recording(1).mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5531872>
+//[Accessed Date 11 May 2026].
+
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260416_180148-Meeting Recording.mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5777373>
+//[Accessed Date 11 May 2026].
--- app/src/main/java/com/example/popcorn_movie_rating_app/displayRatingScreen.kt
+++ app/src/main/java/com/example/popcorn_movie_rating_app/displayRatingScreen.kt
@@ -2,36 +2,154 @@ package com.example.popcorn_movie_rating_app
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class displayRatingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_display_rating_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //Declare the constant value of the img view of the stars used to display rating
+        val oneStar = findViewById<ImageView>(R.id.img1Star)//(Mishka Dewlok,2026)
+        val twoStar = findViewById<ImageView>(R.id.img2Star)//(Mishka Dewlok,2026)
+        val threeStar = findViewById<ImageView>(R.id.img3Star)//(Mishka Dewlok,2026)
+        val fourStar = findViewById<ImageView>(R.id.img4Star)//(Mishka Dewlok,2026)
+        val fiveStar = findViewById<ImageView>(R.id.img5Star)//(Mishka Dewlok,2026)
+        val sixStar = findViewById<ImageView>(R.id.img6)//(Mishka Dewlok,2026)
+        val sevenStar = findViewById<ImageView>(R.id.img7Star)//(Mishka Dewlok,2026)
+        val eighthStar = findViewById<ImageView>(R.id.img8Star)//(Mishka Dewlok,2026)
+        val nineStar = findViewById<ImageView>(R.id.img9Star)//(Mishka Dewlok,2026)
+        val tenStar = findViewById<ImageView>(R.id.img10Star)//(Mishka Dewlok,2026)
+
+
         //Back btn
-        val back = findViewById<Button>(R.id.btnBack)
+        val back = findViewById<Button>(R.id.btnBack)//(Mishka Dewlok,2026)
+
 
         //PROCESSING - Goes back to the main form
         back.setOnClickListener {
             finish()
-        }
+        }//(Mishka Dewlok,2026)
         //Display the name of the movie
-        val nameDisplay = findViewById<TextView>(R.id.txtMovieName)
+        val nameDisplay = findViewById<TextView>(R.id.txtMovieName)//(Mishka Dewlok,2026)
 
         //get  the name from bundle
         val bundle: Bundle? = intent.extras
-        val displayMovieName: String?=bundle?.getString("movieNameDisplay")
+        val displayMovieName: String?=bundle?.getString("movieNameDisplay")//(Mishka Dewlok,2026)
         nameDisplay.text = displayMovieName
+        //bundle- get rating
+        val displayRating: String?=bundle?.getString("rating")//(Mishka Dewlok,2026)
+        //PROCESSING - Determine how much the rating is
+        var rating: Int
+        //convert the rating from string to int
+        rating = displayRating.toString().toInt()//(Mishka Dewlok,2026)
+        //determine the rating and display the rating using stars
+        if (rating==1){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating== 2) {
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 3) {
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 4){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 5){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+            fiveStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 6){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+            fiveStar.isVisible = true //(Mishka Dewlok,2026)
+            sixStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 7){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+            fiveStar.isVisible = true //(Mishka Dewlok,2026)
+            sixStar.isVisible = true //(Mishka Dewlok,2026)
+            sevenStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 8){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+            fiveStar.isVisible = true //(Mishka Dewlok,2026)
+            sixStar.isVisible = true //(Mishka Dewlok,2026)
+            sevenStar.isVisible = true //(Mishka Dewlok,2026)
+            eighthStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 9){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+            fiveStar.isVisible = true //(Mishka Dewlok,2026)
+            sixStar.isVisible = true //(Mishka Dewlok,2026)
+            sevenStar.isVisible = true //(Mishka Dewlok,2026)
+            eighthStar.isVisible = true //(Mishka Dewlok,2026)
+            nineStar.isVisible = true //(Mishka Dewlok,2026)
+        } else if (rating == 10){
+            oneStar.isVisible = true //(Mishka Dewlok,2026)
+            twoStar.isVisible = true //(Mishka Dewlok,2026)
+            threeStar.isVisible = true //(Mishka Dewlok,2026)
+            fourStar.isVisible = true //(Mishka Dewlok,2026)
+            fiveStar.isVisible = true //(Mishka Dewlok,2026)
+            sixStar.isVisible = true //(Mishka Dewlok,2026)
+            sevenStar.isVisible = true //(Mishka Dewlok,2026)
+            eighthStar.isVisible = true //(Mishka Dewlok,2026)
+            nineStar.isVisible = true //(Mishka Dewlok,2026)
+            tenStar.isVisible = true //(Mishka Dewlok,2026)
+        } //(Mishka Dewlok,2026)
+
+    //References - Code Attribution
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260305_180248-Meeting Recording.mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5411078>
+//[Accessed Date 11 May 2026].
+
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260319_180645-Meeting Recording.mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5488548>
+//[Accessed Date 11 May 2026].
+
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260402_181545-Meeting Recording(1).mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5531872>
+//[Accessed Date 11 May 2026].
+
+//Mishka Dewlok,M.D., 2026.
+//IMAD5112 Online 2026-20260416_180148-Meeting Recording.mp4,
+// IMAD5112, Introduction to Mobile Application Development IMAD5112 2026 PT HMAW0501 EMGPON Term 1 GR01
+// [online via internal Virtual Learning Environment (VLE)].
+// The Independent Institute of Education: Unpublished.
+//Available at: <https://mystudies.iie.edu.za/d2l/le/lessons/66661/topics/5777373>
+//[Accessed Date 11 May 2026].
 
 
     }
